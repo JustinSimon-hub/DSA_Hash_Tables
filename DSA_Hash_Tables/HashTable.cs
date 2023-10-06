cb35b41625dac8027695d394a8c0f0a53bf1eb8a
Justin Simon <justinsimon@Justins-MBP.lan>
2023-10-06 00:26:58 -0700

    made tutorial for hash tables with example

---
+++ DSA_Hash_Tables/HashTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace DSA_Hash_Tables
+{
+	public class HashTable
+	{
+		public string[] _hashTable { get; set; }
+		public HashTable()
+		{
+			_hashTable = new string[10];
+		}
+		//***convention to put the private methods at the top above public methods
+		//this is a VERY VERY weak hashing algo, for educational purposes dont use in real environment
+
+		private int _hash(string key)
+		{
+			return key.Length % _hashTable.Length;
+		}
+		public string Get(string key)
+		{
+			int hashedKey = _hash(key);
+			return _hashTable[hashedKey];
+		}
+
+		public void Set(string key , string value)
+		{
+			//key is going to be hashed 
+			int hashedKey = _hash(key);
+			if (_hashTable[hashedKey] != null)
+			{
+				Console.WriteLine("sorry hash collision has occurred");
+			}
+			else
+			{
+				_hashTable[hashedKey] = value;
+			}
+
+		}
+	}
+}
+
---
+++ DSA_Hash_Tables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using DSA_Hash_Tables;
+
+HashTable hashTable = new HashTable();
+
+hashTable.Set("Justin", "661-234-1313");
+hashTable.Set("Jayden", "661-131-4411");
+hashTable.Set("Jessica", "661-313-1341");
+hashTable.Set("Sarah", "661-913-4625");
+hashTable.Set("Batman", "661-141-1451");
+
+
+Console.WriteLine(hashTable.Get("Justin"));
+Console.WriteLine(hashTable.Get("Jayden"));
+Console.WriteLine(hashTable.Get("Jessica"));
+Console.WriteLine(hashTable.Get("Sarah"));
+Console.WriteLine(hashTable.Get("Batman"));
+
+
+
+
